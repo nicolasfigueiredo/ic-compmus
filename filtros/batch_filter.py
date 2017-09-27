190b58950763fff05ea0c3289da1413b98aa0379
nicolasfigueiredo <nicosfigueiredo@gmail.com>
2017-09-27 16:24:20 -0300

    alterações no contraste espectral

--- filtros/batch_filter.py
+++ filtros/batch_filter.py
@@ -1,13 +1,19 @@
 import os
 import librosa
+import pr_util as util
+
 from my_filters import *
 
-audiofiles = [x for x in os.listdir() if (x.endswith('.wav') or x.endswith('.mp3') or x.endswith('.flac') or x.endswith('.aiff')) or x.endswith('.aac')]
+data_dirs = ['/home/nicolas/documentos/compmus/birds/db-tcc-felipe/']
 
-for file in audiofiles:
-  y, sr = librosa.load(file, sr=44100)
-  y_filtered = my_filter(y, sr)
-  path = file + 'filt_exp1.wav'
-
-  librosa.output.write_wav(path, y_filtered, sr)
-  print('Filtered ' + file)
+for data_dir in data_dirs:
+  for subdir, dirs, files in os.walk(data_dir):
+    for file in files:
+      if util.is_audio(file) and file.count('filtered') == 0:
+        file_dir = subdir + '/' + file
+        print(file_dir)
+        y, sr = librosa.load(file_dir, sr=44100)
+        y_filtered = my_filter(y, sr)
+        path = file_dir + '.filtered.wav'
+        print("arquivo filtrado: {}".format(path))
+        librosa.output.write_wav(path, y_filtered, sr)
